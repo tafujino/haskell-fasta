a0123abc30a444a4183632a25ae2e0d994f618d1
Takeshi Fujino <fujino@edu.k.u-tokyo.ac.jp>
2018-08-21 11:57:47 +0900

    REFACTOR: simplify qualified names

--- lib/Bio/Fasta/Parse.hs
+++ lib/Bio/Fasta/Parse.hs
@@ -4,31 +4,32 @@ module Bio.Fasta.Parse
   )
 where
 
-import qualified Data.Attoparsec.ByteString.Char8 as A8
+import Prelude hiding (takeWhile)
+import Data.Attoparsec.ByteString.Char8
 import Control.Monad
 import Data.Attoparsec.Applicative
-import Data.ByteString.Char8 as B8
+import qualified Data.ByteString.Char8 as B8
 import Data.Text.Encoding
 import qualified Data.Text as T
 import Bio.Fasta.Fasta
 
-nameP :: A8.Parser T.Text
-nameP = decodeUtf8 <$> A8.takeWhile1 (not <$> A8.isSpace)
+nameP :: Parser T.Text
+nameP = decodeUtf8 <$> takeWhile1 (not <$> isSpace)
 
-descP :: A8.Parser T.Text
-descP = T.stripEnd  . decodeUtf8 <$> A8.takeWhile (not <$> isEndOfLine')
+descP :: Parser T.Text
+descP = T.stripEnd . decodeUtf8 <$> takeWhile (not <$> isEndOfLine')
 
 -- input characters should be checked in the downstream
-seqP :: A8.Parser ByteString
+seqP :: Parser B8.ByteString
 seqP = B8.concat <$>
-       A8.many1 (A8.takeWhile1 (not <$> A8.isSpace) <* skipSpace' <* A8.endOfLine)
+       many1 (takeWhile1 (not <$> isSpace) <* skipSpace' <* endOfLine)
 
-fasta1Parser :: A8.Parser Fasta
+fasta1Parser :: Parser Fasta
 fasta1Parser = Fasta <$>
                -- unstrict FASTA may contain spaces after '>'
-               (A8.char '>' *> skipSpace' *> nameP) <* skipSpace' <*>
-               descP <* A8.endOfLine <*>
+               (char '>' *> skipSpace' *> nameP) <* skipSpace' <*>
+               descP <* endOfLine <*>
                seqP
 
-fastaParser :: A8.Parser [Fasta]
-fastaParser = A8.many1 fasta1Parser <* A8.skipSpace <* A8.endOfInput
+fastaParser :: Parser [Fasta]
+fastaParser = many1 fasta1Parser <* skipSpace <* endOfInput
