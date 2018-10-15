990463d5dae919eab4c8965f55b7e331009804a1
Takeshi Fujino <fujino@edu.k.u-tokyo.ac.jp>
2018-10-15 15:01:39 +0900

    FEAT: add more IO functions

--- lib/Bio/Fasta.hs
+++ lib/Bio/Fasta.hs
@@ -2,10 +2,17 @@ module Bio.Fasta
 where
 
 import Bio.BioSeq
-import Data.Text
+import qualified Data.ByteString.Char8 as B8
+import qualified Data.Text as T
+import Data.Text.Encoding
 
 data Fasta a = Fasta {
-  fastaName :: Text,
-  fastaDesc :: Text,
+  fastaName :: T.Text,
+  fastaDesc :: T.Text,
   fastaSeq  :: a
   } deriving Show
+
+toByteString :: BioSeq a => Fasta a -> B8.ByteString
+toByteString fa = B8.unlines [encodeUtf8 $ '>' `T.cons` fastaName fa `T.snoc` ' ' `T.append` fastaDesc fa,
+                              toIupacByteString $ fastaSeq fa
+                             ]
--- lib/Bio/Fasta/IO.hs
+++ lib/Bio/Fasta/IO.hs
@@ -1,13 +1,32 @@
 module Bio.Fasta.IO
-  (readFastaFile
+  (readFastaFile,
+   readFastaStdin,
+   writeFastaFile,
+   writeFastaStdout
   )
 where
 
 import Conduit
-import Data.Conduit.Attoparsec
 import Bio.BioSeq
 import Bio.Fasta
 import Bio.Fasta.Parse
+import qualified Data.ByteString.Char8 as B
+import Data.Conduit.Attoparsec
 
 readFastaFile :: BioSeq a => FilePath -> IO [Fasta a]
-readFastaFile path = runConduitRes $ sourceFileBS path .| sinkParser fastaParser
+readFastaFile = readFasta . sourceFileBS
+
+readFastaStdin :: BioSeq a => IO [Fasta a]
+readFastaStdin = readFasta stdinC
+
+readFasta :: BioSeq a => ConduitT () B.ByteString (ResourceT IO) () -> IO [Fasta a]
+readFasta res = runConduitRes $ res .| sinkParser fastaParser
+
+writeFastaFile :: BioSeq a => FilePath -> [Fasta a] -> IO ()
+writeFastaFile = writeFasta . sinkFileBS
+
+writeFastaStdout :: BioSeq a => [Fasta a] -> IO ()
+writeFastaStdout = writeFasta stdoutC
+
+writeFasta :: BioSeq a => ConduitT B.ByteString Void (ResourceT IO) () -> [Fasta a] -> IO ()
+writeFasta res fas = runConduitRes $ yieldMany fas .| mapC toByteString .| res
